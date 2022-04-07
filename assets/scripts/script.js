f7f37203f5ffecc6b10578d53e0441a39bbc13f1
Marlon Tavares <marlontf@gmail.com>
2022-04-07 11:43:12 -0300

    Shuffling cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,7 +14,29 @@ let techs = [
    'react'
 ]
 
-crateCardsFromTechs(techs)
+let cards = null;
+
+startGame()
+
+function startGame() {
+   let cards = crateCardsFromTechs(techs)
+   shuffleCards(cards)
+   console.log(cards)
+}
+
+function shuffleCards(cards) {
+   let currentIndex = cards.length
+   let randomIndex = 0;
+
+   while (currentIndex != 0) {
+      //Gerando um número aleatório MENOR que o índice atual
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex--
+
+      //Invertendo valores do índice atual com o índice gerado
+      [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+   }
+}
 
 function crateCardsFromTechs() {
    let cards = [];
@@ -42,6 +64,6 @@ function createPairFromTech(tech) {
    ]
 }
 
-function createIdWithTech(tech){
+function createIdWithTech(tech) {
    return tech + parseInt(Math.random() * 1000)
 }
